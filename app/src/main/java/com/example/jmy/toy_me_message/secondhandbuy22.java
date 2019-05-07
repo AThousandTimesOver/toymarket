17ee5a724d73d2703ccdf78ef6e75f9f2f4cd354
guozhanhong <1844363279@qq.com>
2019-05-07 17:00:38 +0800

    完善一二手界面及功能

---
+++ app/src/main/java/com/example/jmy/toy_me_message/secondhandbuy22.java
@@ -0,0 +1,13 @@
+package com.example.jmy.toy_me_message;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class secondhandbuy22 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondhandbuy22);
+    }
+}
---
+++ app/src/main/java/com/example/jmy/toy_me_message/secondhandbuy51.java
@@ -0,0 +1,13 @@
+package com.example.jmy.toy_me_message;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class secondhandbuy51 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondhandbuy51);
+    }
+}
